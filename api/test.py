18a42c22b0085078c36a4a57c6933f5d8384435e
Chang Xu <changxu6@illinois.edu>
2022-04-25 15:08:39 -0500

    add readme & clean up test

--- api/test.py
+++
@@ -1,6 +0,0 @@
-from stockfish import Stockfish
-
-stockfish = Stockfish(path= r"stockfish.exe")
-# stockfish.set_depth(51)
-stockfish.set_fen_position("rnbqkbnr/pppp1ppp/8/4p3/7P/7R/PPPPPPP1/RNBQKBN1 b Qkq - 1 2")
-print(stockfish.get_evaluation())
