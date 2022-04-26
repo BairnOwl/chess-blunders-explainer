febc2ee9a269e117cd8a3f260813b124a48381f6
Chang Xu <changxu6@illinois.edu>
2022-04-26 10:30:51 -0500

    enable cors

--- api/app/index.py
+++ api/app/index.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 from util import BlunderExplainer
 
 app = Flask(__name__)
+CORS(app)
 explainer = BlunderExplainer("stockfish.exe")
 
 @app.route("/analysis", methods=['GET'])
